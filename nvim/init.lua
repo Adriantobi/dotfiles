936f98aaf13f6bfe184249de835faebb20a62aa0
Adrian Tobi <40503266+Adriantobi@users.noreply.github.com>
2024-03-29 01:08:00 +0000

    LazyGit

--- nvim/init.lua
+++ nvim/init.lua
@@ -178,6 +178,27 @@ require('lazy').setup({
   },
 
   {
+    --LazyGit
+    "kdheepak/lazygit.nvim",
+  	cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+    	"LazyGitCurrentFile",
+  		"LazyGitFilter",
+  		"LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+
+  {
     -- Trouble Diagnostics
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
