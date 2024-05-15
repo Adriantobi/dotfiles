5ac515ccca4e184832b083d4d4a741feb17b8c72
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-05-15 12:21:44 +0100

    Fix mason

--- nvim/lua/user/plugins/lsp/mason.lua
+++ nvim/lua/user/plugins/lsp/mason.lua
@@ -9,9 +9,6 @@ return {
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-    -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
   },
   config = function()
     local mason = require("mason")
