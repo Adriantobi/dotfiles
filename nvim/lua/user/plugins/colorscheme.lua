4a7cdb85dc795363644c5e6713335e87b1c0c39f
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-05-16 09:30:13 +0100

    colorscheme to catppuccin

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -1,9 +1,25 @@
 return {
-    -- Hybrid Theme
-    'vimoxide/vim-cinnabar',  -- Other themes: '', 'alexanderbluhm/black.nvim', 'catppuccin/nvim', 'AlexvZyl/nordic.nvim', 'felipeagc/fleet-theme-nvim'
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'cinnabar'
-    end,
+	-- Hybrid Theme
+	"catppuccin/nvim", -- Other themes: 'vimoxide/vim-cinnabar', 'alexanderbluhm/black.nvim', 'HoNamDuong/hybrid.nvim', HoNamDuong/hybrid.nvim'AlexvZyl/nordic.nvim', 'felipeagc/fleet-theme-nvim'
+	lazy = false,
+	priority = 1000,
+	name = "catppuccin",
+	config = function()
+		local theme = require("catppuccin")
+
+		theme.setup({
+			flavour = "mocha",
+			term_colors = true,
+			transparent_background = false,
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		})
+
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
