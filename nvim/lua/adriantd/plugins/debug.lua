9edd2fe66f9303a99c5e2065437cb426c211b037
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-05-21 13:08:45 +0100

    Debug signs

--- nvim/lua/adriantd/plugins/debug.lua
+++ nvim/lua/adriantd/plugins/debug.lua
@@ -20,6 +20,7 @@ return {
 		})
 
 		local keymap = vim.keymap
+		local highlight = vim.api.nvim_set_hl
 
 		keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
 		keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
@@ -52,5 +53,31 @@ return {
 		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+		highlight(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+		highlight(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+		highlight(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+		vim.fn.sign_define(
+			"DapBreakpoint",
+			{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointRejected",
+			{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapLogPoint",
+			{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+		)
+		vim.fn.sign_define(
+			"DapStopped",
+			{ text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+		)
+		vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapRed", linehl = "", numhl = "" })
 	end,
 }
