13f6bb724da00e24a194454d83228bc36f29c3c6
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-06-07 16:00:54 +0100

    Dadbod keybindings

--- nvim/lua/adriantd/plugins/dadbod.lua
+++ nvim/lua/adriantd/plugins/dadbod.lua
@@ -1,5 +1,18 @@
 return {
-	"tpope/vim-dadbod",
-	"kristijanhusak/vim-dadbod-completion",
 	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		"tpope/vim-dadbod",
+		"kristijanhusak/vim-dadbod-completion",
+	},
+	config = function()
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>dd", "<cmd>DBUIToggle<cr>", { noremap = true, silent = true, desc = "Toggle dadbod" })
+		keymap.set(
+			"n",
+			"<leader>da",
+			"<cmd>DBUIAddConnection<cr>",
+			{ noremap = true, silent = true, desc = "Add dadbod connection" }
+		)
+	end,
 }
