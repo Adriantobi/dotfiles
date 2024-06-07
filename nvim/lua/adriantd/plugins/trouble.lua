75eb37f61605bc6b9f8c02e369e06e06bef8cd52
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-06-07 11:23:04 +0100

    Trouble update

--- nvim/lua/adriantd/plugins/trouble.lua
+++ nvim/lua/adriantd/plugins/trouble.lua
@@ -5,25 +5,14 @@ return {
 
 	config = function()
 		local trouble = require("trouble")
+		trouble.setup({})
+
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>xx", function()
-			trouble.toggle()
-		end, { desc = "Toggle Trouble" })
-		keymap.set("n", "<leader>xw", function()
-			trouble.toggle("workspace_diagnostics")
-		end, { desc = "Trouble Workspace Diagnostics" })
-		keymap.set("n", "<leader>xd", function()
-			trouble.toggle("document_diagnostics")
-		end, { desc = "Trouble Document Diagnostics" })
-		keymap.set("n", "<leader>xq", function()
-			trouble.toggle("quickfix")
-		end, { desc = "Trouble Quickfix" })
-		keymap.set("n", "<leader>xl", function()
-			trouble.toggle("loclist")
-		end, { desc = "Toggle Trouble Loclist" })
-		keymap.set("n", "gR", function()
-			trouble.toggle("lsp_references")
-		end, { desc = "Trouble LSP References" })
+		keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle Trouble" })
+		keymap.set("n", "<leader>xb",	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",	{ desc = "Buffer Diagnostics (Trouble)" })
+		keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { desc = "Trouble Quickfix" })
+		keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", { desc = "Toggle Trouble Loclist" })
+		keymap.set("n",	"<leader>xR", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "Trouble LSP References" })
 	end,
 }
