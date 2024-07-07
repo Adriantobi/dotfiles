db16b1aa699a930b0a0b7ee9ea703ba20ffa4401
Adrian Tobi <talktotobi.a@gmail.com>
2024-07-07 19:17:37 +0100

    change colr scheme

--- nvim/lua/adriantd/plugins/colorscheme.lua
+++ nvim/lua/adriantd/plugins/colorscheme.lua
@@ -1,31 +1,31 @@
 return {
 	-- Colorscheme
 	-- 'f-person/auto-dark-mode.nvim'
-	"2nthony/vitesse.nvim", --Other themes: 'catppuccin/nvim', 'projekt0n/github-nvim-theme', 'HoNamDuong/hybrid.nvim', 'vimoxide/vim-cinnabar', 'alexanderbluhm/black.nvim', 'Murtaza-Udaipurwala/gruvqueen', 'AlexvZyl/nordic.nvim', 'felipeagc/fleet-theme-nvim'
+	"catppuccin/nvim", --Other themes: 'sar/extra-darkplus.nvim', '2nthony/vitesse.nvim', 'projekt0n/github-nvim-theme', 'HoNamDuong/hybrid.nvim', 'vimoxide/vim-cinnabar', 'alexanderbluhm/black.nvim', 'Murtaza-Udaipurwala/gruvqueen', 'AlexvZyl/nordic.nvim', 'felipeagc/fleet-theme-nvim'
 	lazy = false,
-	-- name = "catppuccin",
+	name = "catppuccin",
 	priority = 1000,
-	dependencies = {
-		"tjdevries/colorbuddy.nvim",
-	},
+	-- dependencies = {
+	-- 	"tjdevries/colorbuddy.nvim",
+	-- },
 	config = function()
-		local theme = require("vitesse")
+		local theme = require("catppuccin")
 
-		-- theme.setup({
-		-- 	flavour = "mocha",
-		-- 	term_colors = true,
-		-- 	transparent_background = false,
-		-- 	no_italic = false,
-		-- 	no_bold = false,
-		-- 	color_overrides = {
-		-- 		mocha = {
-		-- 			base = "#000000",
-		-- 			mantle = "#000000",
-		-- 			crust = "#000000",
-		-- 		},
-		-- 	},
-		-- })
+		theme.setup({
+			flavour = "mocha",
+			term_colors = true,
+			transparent_background = false,
+			no_italic = false,
+			no_bold = false,
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		})
 
-		vim.cmd.colorscheme("vitesse")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
