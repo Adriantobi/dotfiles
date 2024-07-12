6187eb453be7cdf248878f02489db199240b27c6
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-07-12 15:12:48 +0100

    Update nvim-silicon.lua for keybinding

--- nvim/lua/adriantd/plugins/nvim-silicon.lua
+++ nvim/lua/adriantd/plugins/nvim-silicon.lua
@@ -10,18 +10,25 @@ return {
 	},
 	config = function()
 		local silicon = require("nvim-silicon")
+		local keymap = vim.keymap
 
 		silicon.setup({
 			font = "JetBrainsMono Nerd Font=34",
 			window_title = function()
 				return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
 			end,
+			language = function()
+				return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":e")
+			end,
 			output = function()
 				return vim.fn.expand("$HOME")
 					.. "\\Pictures\\Screenshots\\"
 					.. os.date("!%Y-%m-%dT%H-%M-%SZ")
 					.. "_code.png"
-			end,
+				end,
+			to_clipboard = true,
 		})
+
+		keymap.set("v", "<S-s>", ":'<,'>Silicon<cr>", { noremap = true, silent = true })
 	end,
 }
