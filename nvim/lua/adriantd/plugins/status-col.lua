736dbb4b2577e40af483f80117ad26f446caf41d
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-07-12 13:26:44 +0100

    Create status-col.lua

---
+++ nvim/lua/adriantd/plugins/status-col.lua
@@ -0,0 +1,20 @@
+return {
+  'luukvbaal/statuscol.nvim',
+  opts = function()
+    local builtin = require('statuscol.builtin')
+    return {
+      setopt = true,
+      -- override the default list of segments with:
+      -- number-less fold indicator, then signs, then line number & separator
+      segments = {
+        { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+        { text = { '%s' }, click = 'v:lua.ScSa' },
+        {
+          text = { builtin.lnumfunc, ' ' },
+          condition = { true, builtin.not_empty },
+          click = 'v:lua.ScLa',
+        },
+      },
+    }
+  end,
+}
