0c4962e6525d5c60e1d5be9b6a3c286b8f52cc11
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-07-17 12:50:35 +0100

    Update lspconfig.lua

--- nvim/lua/adriantd/plugins/lsp/lspconfig.lua
+++ nvim/lua/adriantd/plugins/lsp/lspconfig.lua
@@ -83,7 +83,6 @@ return {
 		}
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
