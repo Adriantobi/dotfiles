dd735ed603f11d4c17893439bf1a12df1b42dd78
Adrian Tobi 王拓 <40503266+Adriantobi@users.noreply.github.com>
2024-10-28 17:43:20 +0000

    undo tree diff fix

--- nvim/lua/adriantd/plugins/undo-tree.lua
+++ nvim/lua/adriantd/plugins/undo-tree.lua
@@ -5,6 +5,7 @@ return {
 		local keymap = vim.keymap
 
 		keymap.set("n", "<leader>u", ":OutlineClose<CR>:DBUIClose<CR>:UndotreeToggle<CR>", { desc = "UndotreeToggle" })
+		vim.g.undotree_DiffCommand = "FC"
 		vim.g.undotree_SetFocusWhenToggle = true
 	end,
 }
