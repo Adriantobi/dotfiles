58cddf96c226119d856b41ab7f84d5e331dbe715
Adrian Tobi <talktotobi.a@gmail.com>
2026-09-07 14:48:19 +0100

    fix minor typo

--- nvim/lua/adriantd/core/options.lua
+++ nvim/lua/adriantd/core/options.lua
@@ -59,7 +59,7 @@ autocmd("BufReadPost", {
 	desc = "Jump to last pos when opening a file",
 	callback = function(args)
 		local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
-		local not_commit = vim.b[args.buff].filetype ~= "commit"
+		local not_commit = vim.b[args.buf].filetype ~= "commit"
 
 		if valid_line and not_commit then
 			vim.cmd([[normal! g`"]])
